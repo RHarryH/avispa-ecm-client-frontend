483b4828719943311b315863ba30d21c3aedf806
Rafał Hiszpański <rharryh@gmail.com>
2023-07-06 13:25:48 +0200

    Added dynamic meta description and title Added favicon Header and footer are moved from the previous app

--- src/Header.tsx
+++ src/Header.tsx
@@ -20,28 +20,75 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useEffect, useState} from "react";
 
 function Header() {
+    const [headerData, setHeaderData] = useState({
+        brand: "Avispa ECM Client",
+        menuItems: [{
+            name: "",
+            actions: [{name: "", type: ""}]
+        }]
+    });
+
+    useEffect(() => {
+        return setHeaderData({
+            brand: "μF",
+            menuItems: [
+                {
+                    name: "Invoice",
+                    actions: [
+                        {
+                            name: "Add new",
+                            type: "invoice-add-button"
+                        },
+                        {
+                            name: "Clone",
+                            type: 'invoice-clone-button'
+                        }
+                    ]
+                },
+                {
+                    name: "Customer",
+                    actions: [
+                        {
+                            name: "Add new",
+                            type: "customer-add-button"
+                        }
+                    ]
+                },
+                {
+                    name: "Bank account",
+                    actions: [
+                        {
+                            name: "Add new",
+                            type: "bank-account-add-button"
+                        }
+                    ]
+                }
+            ]
+        });
+    }, []);
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar expand="md" fixed="top" bg="dark" data-bs-theme="dark" aria-label="Navigation bar">
+            <Container fluid>
+                <Navbar.Brand href="#home">{headerData.brand}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" aria-expanded="false" aria-label="Toggle navigation"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        {
+                            headerData.menuItems.map(item => (
+                                <NavDropdown title={item.name} id="basic-nav-dropdown">
+                                    {
+                                        item.actions.map(action => (
+                                            <NavDropdown.Item className={action.type}
+                                                              href="#">{action.name}</NavDropdown.Item>
+                                        ))
+                                    }
+                                </NavDropdown>
+                            ))
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
